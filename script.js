f7c470d85e7855bacd8e3cd0bafc18a7cc507dc9
Rogerio-Lacerda <rogerio.barrosg@gmail.com>
2022-01-19 11:44:48 -0300

    Desafio de lógica, usando for/while.

---
+++ script.js
@@ -0,0 +1,34 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+console.log("1)-----");
+
+for(let i = 1;i <= 10;i++){
+  console.log(i);
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+console.log("2)-----");
+
+let cont = 10;
+
+while(cont >= 1){
+  console.log(cont);
+  cont --;
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+console.log("3)------");
+
+for(let i = 1;i < 100;i+=2){
+  console.log(i);
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+console.log("4)------");
+
+for(let i = 0;i <= 100;i+=2){
+  console.log(i);
+}
